bca29f8643ddd2a51797dcdcf22187d73a423290
s16009 <s16009@std.it-college.ac.jp>
2017-10-19 14:09:36 +0900

    first commit

--- 1018 課題/1018.4.sql
+++ 1018 課題/1018.4.sql
@@ -1,4 +1,4 @@
 select last_name, department_id, job_id
-from employees natural join departments
-where location_id = 1700
+from employees
+where department_id in (select department_id from departments where location_id = 1700)
 /
